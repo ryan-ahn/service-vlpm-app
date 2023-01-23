1c173d8f4b89231770b840e6d86164e41491ad88
Ryan <kaaiinn4@gmail.com>
2023-01-24 03:58:02 +0900

    Add: ios firebase

--- ios/vlpmclientsrelease/AppDelegate.mm
+++ ios/vlpmclientsrelease/AppDelegate.mm
@@ -3,6 +3,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNSplashScreen.h>
+#import <Firebase.h>
 
 #import <React/RCTAppSetupUtils.h>
 
@@ -29,8 +31,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   RCTAppSetupPrepareApp(application);
-
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
 #if RCT_NEW_ARCH_ENABLED
@@ -54,6 +56,8 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  // Splash
+  [RNSplashScreen show];
   return YES;
 }
 
